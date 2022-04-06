f3b0d48632462282c52c4ae7cc5dd06f626a3550
alexeenko <i.html06@gmail.com>
2022-04-07 00:03:16 +0600

    feat: add some functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-$.modal()
+const modal = $.modal({
+  title: 'Alexeyenko Artyom',
+  closable: true,
+  content: `
+  <p>lorem</p>`,
+  width: '400px',
+  footerButtons: [
+    {text: 'Ok', type: 'primary', handler() {
+      modal.close()
+    }},
+    {text: 'Cancel', type: 'danger', handler() {
+      modal.close()
+    }},
+  ]
+})
